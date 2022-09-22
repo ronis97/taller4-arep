b0f216b11535abe451feb6396fe3796695144f1a
unknown <edgar.henao@labinfo.is.escuelaing.edu.co>
2022-09-22 15:29:16 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/arep/SparkWebServer.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arep;
+
+import spark.Request;
+import spark.Response;
+
+import static spark.Spark.*;
+public class SparkWebServer
+{
+    public static void main(String... args){
+        port(getPort());
+        get("hello", (req,res) -> "Hello Docker!");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
